5b4b960cc43ae88e17b05e0ec1b21ec46af10d2b
Bahrican Akkoyun <bahricanakkoyun@gmail.com>
2019-01-24 23:10:59 +0300

    feat: initial push of package

---
+++ README.md
@@ -0,0 +1,32 @@
+# Paramoia
+
+[![](https://img.shields.io/npm/v/paramoia.svg?style=flat-square)](https://www.npmjs.com/package/paramoia) [![](https://img.shields.io/lgtm/grade/javascript/g/b8kkyn/paramoia.svg?style=flat-square&logo=lgtm&logoWidth=18)](https://lgtm.com/projects/g/b8kkyn/paramoia/context:javascript) [![](https://img.shields.io/lgtm/alerts/g/b8kkyn/paramoia.svg?style=flat-square&logo=lgtm&logoWidth=18)](https://lgtm.com/projects/g/b8kkyn/paramoia/alerts/) [![](https://img.shields.io/badge/code_style-standard-brightgreen.svg?style=flat-square)](https://standardjs.com) [![](https://img.shields.io/github/license/b8kkyn/paramoia.svg?style=flat-square)](https://github.com/b8kkyn/paramoia/blob/master/LICENSE)
+
+Cure for being paranoia about parameters which are coming in every single wrong way to our functions.
+
+## What is actually?
+
+Well, it is simple utility library for parameter checking.
+
+## Who is for?
+
+- If you don't want to spare time for understand other _JSON or OBJECT SCHEMA_ based validator libraries,
+- If you don't want to deal with complex _JSON or OBJECT STRUCTURES_ for being able to validate some parameters,
+- If you're more like pass or fail line by line kind a guy,
+- If you want simple, light, easy-to-understand library,
+
+You could use this library.
+
+## How to install?
+
+Run `npm install paramoia --save` then you're ready to go with:
+
+`let Paramoia = require('paramoia')`
+
+## Api
+
+[Go API documentation page](https://github.com/b8kkyn/paramoia/blob/master/API.md)
+
+## Changelog
+
+[Go CHANGELOG](https://github.com/b8kkyn/paramoia/blob/master/CHANGELOG.md)
---
+++ index.js
@@ -0,0 +1,304 @@
+/**
+ * @typedef {Object} module:Paramoia.Types
+ * @property Number {Number} Number type.
+ * @property String {Number} String type.
+ * @property Boolean {Number} Boolean type.
+ * @property Array {Number} Array type.
+ * @property Object {Number} Object type.
+ * @property Null {Number} Null type.
+ * @property Undefined {Number} Undefined type.
+ */
+
+/**
+ * @typedef {Object} module:Paramoia.Include
+ * @property Only {Number} Only condition.
+ * @property Must {Number} Must condition.
+ * @property MustOnly {Number} MustOnly condition.
+ */
+
+/**
+ * Cure for being paranoia about parameters which are coming in every single wrong way to our functions.
+ *
+ * @module Paramoia
+ */
+module.exports = class Paramoia {
+  static get Types () {
+    return {
+      Number: 1,
+      String: 2,
+      Boolean: 3,
+      Array: 4,
+      Object: 5,
+      Null: 6,
+      Undefined: 7
+    }
+  }
+
+  static get Include () {
+    return {
+      Only: 1,
+      Must: 2,
+      MustOnly: 3
+    }
+  }
+
+  /**
+   * It checks if **param** includes **contain** or not.
+   *
+   * @param {(Array.<String>|Object.<String, *>)} param Parameter which will be checked.
+   * @param {(Array.<String>|Object.<String, *>)} contain Parameter which will be checked against.
+   * @param {Number} [condition=Paramoia.Include.Must] Parameter which indicates check condition:
+   * `Only`: **param** *can include less* than **contain** but different.
+   * `Must`: **param** *must include* **contain**.
+   * `MustOnly`: **param** *must only include* **contain**.
+   * You should use [Paramoia.Include.*]{@link module:Paramoia.Include}
+   *
+   * @returns {Boolean} Result of check.
+   * - `TRUE`: Check condition met.
+   * - `FALSE`: Check condition didn't meet.
+   */
+  static includes (param, contain, condition) {
+    if (condition === Paramoia.Include.Only) {
+      return this._contains(contain, param)
+    } else if (condition === Paramoia.Include.Must) {
+      return this._contains(param, contain)
+    } else if (condition === Paramoia.Include.MustOnly) {
+      return this._contains(param, contain) && this._contains(contain, param)
+    }
+  }
+
+  /**
+   * It checks if **param** equals *at least* one of **types** or not.
+   *
+   * @param {*} param Parameter which will be checked.
+   * @param {(Array.<Number>|...Number)} types Parameter(s) which will be checked against. You should use [Paramoia.Types.*]{@link module:Paramoia.Types}
+   * @returns {Boolean} Result of check.
+   * - `TRUE`: **param** DOES equal *at least* one of **types**.
+   * - `FALSE`: **param** DOES NOT equal any of **types**.
+   */
+  static equals (param, types) {
+    if (!this._isArray(types)) {
+      types = Object.values(arguments)
+      types.shift()
+    }
+    for (let type of types) {
+      if (this._is(param, type)) {
+        return true
+      }
+    }
+    return false
+  }
+
+  /**
+   * It checks if **param** is one of **whitelist** or not.
+   *
+   * @param {Number|String} param Parameter which will be checked.
+   * @param {Array.<(Number|String)>} whitelist Parameter which will be checked against.
+   * @returns {Boolean} Result of check.
+   * - `TRUE`: **param** IS one of **whitelist**.
+   * - `FALSE`: **param** IS NOT one of **whitelist**.
+   */
+  static valid (param, whitelist) {
+    return this.has(whitelist, param)
+  }
+
+  /**
+   * It checks if **param** has **key** or not.
+   *
+   * @param {(Array.<String>|Object.<String, *>)} param Parameter which will be checked.
+   * @param {String} key Parameter which will be checked against.
+   * @returns {Boolean} Result of check.
+   * - `TRUE`: **param** HAS **key**.
+   * - `FALSE`: **param** HAS NOT **key**.
+   */
+  static has (param, key) {
+    if (this._isArray(param)) {
+      return param.includes(key)
+    } else if (this._isObject(param)) {
+      return key in param
+    }
+  }
+
+  /**
+   * It checks if every value of **param** is one of **types** or not.
+   *
+   * @param {(Array.<*>|Object.<String, *>)} param Parameter which will be checked.
+   * @param {(Array.<Number>|...Number)} types Parameter(s) which will be checked against. You should use [Paramoia.Types.*]{@link module:Paramoia.Types}
+   * @returns {Boolean} Result of check.
+   * - `TRUE`: **param** HAS values which are one of **types**.
+   * - `FALSE`: **param** HAS values which are NOT one of **types**.
+   */
+  static values (param, types) {
+    if (!this._isArray(types)) {
+      types = Object.values(arguments)
+      types.shift()
+    }
+    if (this._isObject(param)) {
+      param = Object.values(param)
+    }
+    for (let item of param) {
+      if (!this.equals(item, types)) {
+        return false
+      }
+    }
+    return true
+  }
+
+  /**
+   * It checks if **param** is *empty* or not.
+   *
+   * @param {(String|Array.<*>|Object.<String, *>)} param Parameter which will be checked.
+   * @returns {Boolean} Result of check.
+   * - `TRUE`: **param** IS *empty*.
+   * - `FALSE`: **param** IS NOT *empty*.
+   */
+  static empty (param) {
+    if (this._isString(param)) {
+      return param === ''
+    } else if (this._isArray(param)) {
+      return param.length === 0
+    } else if (this._isObject(param)) {
+      return Object.keys(param).length === 0
+    }
+  }
+
+  /**
+   * It checks if **param** includes **contain** or not.
+   *
+   * @private
+   * @param {(Array.<String>|Object.<String, *>)} param Parameter which will be checked.
+   * @param {(Array.<String>|Object.<String, *>)} contain Parameter which will be checked against.
+   * @returns {Boolean} Result of check.
+   * - `TRUE`: **param** DOES include **contain**.
+   * - `FALSE`: **param** DOES NOT include **contain**.
+   */
+  static _contains (param, contain) {
+    contain = this._isObject(contain) ? Object.keys(contain) : contain
+    for (let item of contain) {
+      if (!this.has(param, item)) {
+        return false
+      }
+    }
+    return true
+  }
+
+  /**
+   * It checks if **param** equals **type** or not.
+   *
+   * @private
+   * @param {*} param Parameter which will be checked.
+   * @param {Number} type Parameter which will be checked against. You should use one of [Paramoia.Types.*]{@link module:Paramoia.Types}
+   * @returns {Boolean} Result of check.
+   * - `TRUE`: **param** DOES equal **type**.
+   * - `FALSE`: **param** DOES NOT equal **type**.
+   */
+  static _is (param, type) {
+    if (type === this.Types.Number) {
+      return this._isNumber(param)
+    } else if (type === this.Types.String) {
+      return this._isString(param)
+    } else if (type === this.Types.Boolean) {
+      return this._isBoolean(param)
+    } else if (type === this.Types.Array) {
+      return this._isArray(param)
+    } else if (type === this.Types.Object) {
+      return this._isObject(param)
+    } else if (type === this.Types.Null) {
+      return this._isNull(param)
+    } else if (type === this.Types.Undefined) {
+      return this._isUndefined(param)
+    }
+    return false
+  }
+
+  /**
+   * It checks if **param** is *number* or not.
+   *
+   * @private
+   * @param {*} param Parameter which will be checked.
+   * @returns {Boolean} Result of check.
+   * - `TRUE`: **param** IS *number*.
+   * - `FALSE`: **param** IS NOT *number*.
+   */
+  static _isNumber (param) {
+    return typeof param === 'number'
+  }
+
+  /**
+   * It checks if **param** is *string* or not.
+   *
+   * @private
+   * @param {*} param Parameter which will be checked.
+   * @returns {Boolean} Result of check.
+   * - `TRUE`: **param** IS *string*.
+   * - `FALSE`: **param** IS NOT *string*.
+   */
+  static _isString (param) {
+    return typeof param === 'string'
+  }
+
+  /**
+   * It checks if **param** is *boolean* or not.
+   *
+   * @private
+   * @param {*} param Parameter which will be checked.
+   * @returns {Boolean} Result of check.
+   * - `TRUE`: **param** IS *boolean*.
+   * - `FALSE`: **param** IS NOT *boolean*.
+   */
+  static _isBoolean (param) {
+    return typeof param === 'boolean'
+  }
+
+  /**
+   * It checks if **param** is *array* or not.
+   *
+   * @private
+   * @param {*} param Parameter which will be checked.
+   * @returns {Boolean} Result of check.
+   * - `TRUE`: **param** IS *array*.
+   * - `FALSE`: **param** IS NOT *array*.
+   */
+  static _isArray (param) {
+    return Array.isArray(param)
+  }
+
+  /**
+   * It checks if **param** is *object* or not.
+   *
+   * @private
+   * @param {*} param Parameter which will be checked.
+   * @returns {Boolean} Result of check.
+   * - `TRUE`: **param** IS *object*.
+   * - `FALSE`: **param** IS NOT *object*.
+   */
+  static _isObject (param) {
+    return param !== null && typeof param === 'object' && !this._isArray(param)
+  }
+
+  /**
+   * It checks if **param** is *null* or not.
+   *
+   * @private
+   * @param {*} param Parameter which will be checked.
+   * @returns {Boolean} Result of check.
+   * - `TRUE`: **param** IS *null*.
+   * - `FALSE`: **param** IS NOT *null*.
+   */
+  static _isNull (param) {
+    return param === null
+  }
+
+  /**
+   * It checks if **param** is *undefined* or not.
+   *
+   * @private
+   * @param {*} param Parameter which will be checked.
+   * @returns {Boolean} Result of check.
+   * - `TRUE`: **param** IS *undefined*.
+   * - `FALSE`: **param** IS NOT *undefined*.
+   */
+  static _isUndefined (param) {
+    return typeof param === 'undefined'
+  }
+}
